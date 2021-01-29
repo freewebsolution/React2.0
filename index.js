2221e6cb18e07215579dc3be6fb2b270b2df0b24
lucio Ticali <info@freewebsolution.it>
2021-01-29 09:07:30 +0100

    Aggiunta backend

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const app = express();
+app.use(express.json())
+
+let heroes = [
+    {
+        "id": 1,
+        "name": "Captain America",
+        "important":true
+    },
+    {
+        "id": 2,
+        "name": "Iron Man",
+        "important": true
+    },
+    {
+        "id": 3,
+        "name": "Hulk",
+        "important": false
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello world</h1>')
+})
+
+app.get('/api/heroes', (request, response) => {
+    response.json(heroes)
+})
+app.get('/api/heroes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const hero = heroes.find(hero => hero.id === id)
+    if(hero){
+       response.json(hero) 
+    } else {
+        response.status(404).end()
+    }
+    
+})
+const generateId =() => {
+    const maxId = heroes.length > 0 ? Math.max(...heroes.map(h => h.id)): 0
+    return maxId + 1
+}
+app.post('/api/heroes', (request, response) => {
+    const body = request.body
+    if(!body.name){
+        return response.status(404).json({
+            error: 'Contenuto vuoto'
+        })
+    }
+
+    const hero = {
+        name: body.name,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+
+    heroes = heroes.concat(hero)
+
+    response.json(hero)
+})
+app.delete('/api/heroes/:id', (request, response)=> {
+    const id = Number(request.params.id)
+    heroes = heroes.filter(hero => hero.id === id)
+    response.status(204).end()
+})
+
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
