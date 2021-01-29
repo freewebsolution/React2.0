68d6ec82ed4254a21acc5555e0d51f23d533e72b
lucio Ticali <info@freewebsolution.it>
2021-01-29 10:11:23 +0100

    Add middleware

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express();
 app.use(express.json())
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
 let heroes = [
     {
         "id": 1,
