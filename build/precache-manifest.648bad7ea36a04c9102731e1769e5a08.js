a7be3a07b1b7b0418cbb68439d467a241efab2fb
lucio Ticali <info@freewebsolution.it>
2021-02-05 18:21:00 +0100

    Fatto la Build

---
+++ build/precache-manifest.648bad7ea36a04c9102731e1769e5a08.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91cb7484033e400757731b186304ba6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb08c65bd36e269d15f2",
+    "url": "/static/js/2.028743c2.chunk.js"
+  },
+  {
+    "revision": "3812b6ff9635b81091c0",
+    "url": "/static/js/main.361ca333.chunk.js"
+  },
+  {
+    "revision": "f4240dd0dfea35ea3093",
+    "url": "/static/js/runtime-main.c99aca5a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 app.use(express.json())
+app.use(express.static('build'))
 
 const cors = require('cors')
 app.use(cors())
