de8f145f64a8253b0acafea32d890b6c5079a9c3
lucio Ticali <info@freewebsolution.it>
2021-02-23 11:24:55 +0100

    Aggiunta es lint

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(cors())
 app.use(express.static('build'))
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method),
-        console.log('Path:', request.path),
-        console.log('Body:', request.body)
+    console.log('Path:', request.path),
+    console.log('Body:', request.body)
     console.log('---')
     next()
 }
